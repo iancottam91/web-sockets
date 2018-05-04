32c0920f025a9c0537ae6878a55573160361c427
Ian Cottam <ian.cottam@bbc.co.uk>
2018-05-04 15:05:25 +0100

    Make it look good

--- notify/models/trigger-trigger.js
+++
@@ -1,9 +0,0 @@
-const pg = require('pg');
-const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/uploads';
-
-const client = new pg.Client(connectionString);
-client.connect();
-
-const query = client.query(
-    "INSERT INTO FOOD(ID, NAME) VALUES ('1', 'a');");
-query.on('end', () => { client.end(); });
